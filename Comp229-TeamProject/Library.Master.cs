9caa2624b5e163360b61c5a6e975df157d8f770b
shilpa <shilpagandhi111@gmail.com>
2016-11-24 22:01:46 -0500

    added page title

--- Comp229-TeamProject/Library.Master.cs
+++ Comp229-TeamProject/Library.Master.cs
@@ -11,7 +11,28 @@ namespace Comp229_TeamProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            var pageUrl = HttpContext.Current.Request.RawUrl;
+            var splitPageUrl = pageUrl.Split('/');
+            switch (splitPageUrl[splitPageUrl.Length - 1])
+            {
+                case "MainTrackingPage.aspx":
+                    Page.Title = "D.S Library";
+
+                    break;
+                case "Services.aspx":
+                    Page.Title = "Services - D.S Library";
+                    break;
+                case "RegisterPage.aspx":
+                    Page.Title = "Register - D.S Library";
+                    break;
+                case "CatalougePage.aspx":
+                    Page.Title = "Catalouges";
+                    break;
+                case "Contact.aspx":
+                    Page.Title = "D.S Library";
+                    break;
+            }
+
         }
     }
 }
